c677f4162c9cda8adc5ca0956f26f8cdf6e3939a
Evanlol123 <116678253+Evanlol123@users.noreply.github.com>
2024-05-20 00:16:35 -0400

    Delete wax/payloads/wifi.sh

--- wax/payloads/wifi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Wings - SH1MMER Wifi Payload"
-echo "Will only work with Open and password-only networks, not EAP networks. Leave password blank for Open networks."
-echo "Made by r58Playz"
-read -rep "network > " network
-read -rep "password> " password
-/usr/local/bin/python3 /usr/local/autotest/client/cros/scripts/wifi connect "$network" "$password"
